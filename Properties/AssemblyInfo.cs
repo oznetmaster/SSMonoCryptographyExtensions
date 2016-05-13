2248e1f5328a4033d2285465a47d9aeeae59e87e
oznetmaster <ncolvin@marvelous.com>
2016-05-13 16:58:07 +0800

    Initial post of S# port of additional mono Cryptography ciphers

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle ("SSMonoCryptographyExtensions")]
+[assembly: AssemblyCompany ("")]
+[assembly: AssemblyProduct ("SSMonoCryptographyExtensions")]
+[assembly: AssemblyCopyright ("Copyright ©  2016")]
+[assembly: AssemblyVersion ("1.0.0.*")]
+
+[assembly: InternalsVisibleTo ("SSMonoSSlStreamLibrary")]
+
